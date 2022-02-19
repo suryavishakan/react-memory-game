4aac8a1b5637ee69a9b25823dd70152636408c66
surya vishakan <suryavishakan050@gmail.com>
2022-02-19 11:27:24 +0530

    created the card grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
 import "./App.css";
+import snapchat from "./assets/snapchat.png";
+import telegram from "./assets/telegram.png";
+import tiktok from "./assets/tiktok.png";
+import twitter from "./assets/twitter.png";
+import whatsapp from "./assets/whatsapp.png";
+import youtube from "./assets/youtube.png";
+import cardBackground from "./assets/back.jpg";
+
+const cardImages = [
+  { src: snapchat },
+  { src: telegram },
+  { src: tiktok },
+  { src: twitter },
+  { src: whatsapp },
+  { src: youtube },
+];
 
 function App() {
-  return <div className="App"></div>;
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+    console.log("shuffled cards", shuffledCards);
+  };
+
+  return (
+    <div className="App">
+      <h2>Magic Match</h2>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div key={card.id} className="card">
+            <div>
+              <img src={card.src} className="front" alt="card front" />
+              <img src={cardBackground} className="back" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
